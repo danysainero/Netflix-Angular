4fef0364f4e3c14601dc82fb0716a24eb6302577
Carlos Prieto <carlospm86@gmail.com>
2019-06-01 00:31:02 +0200

    test_01 video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoComponent } from './shared/components/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    VideoComponent,
   ],
   imports: [
     BrowserModule,
