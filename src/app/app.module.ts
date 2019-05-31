3117bd7a4da24e5e0a0996df9f66edd2c27fff5e
Carlos Prieto <carlospm86@gmail.com>
2019-05-31 19:41:45 +0200

    Revert "prueba video"
    This reverts commit a9d69a1ca65a3b82dd55aa955fd4907700cb7101.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VideoComponent } from './shared/components/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoComponent,
   ],
   imports: [
     BrowserModule,
