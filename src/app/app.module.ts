70690884fcc1b18e5a2d0cdb928b6c9c74f45820
Cris <csimonvalero@gmail.com>
2019-05-31 17:19:57 +0200

    borrado prueba component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './shared/prueba/prueba.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
   ],
   imports: [
     BrowserModule,
