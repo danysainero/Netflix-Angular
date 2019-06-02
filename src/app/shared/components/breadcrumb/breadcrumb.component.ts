ba59232d07c49952a00f807d91c6138dbd66591e
danysainero <danysainero@gmail.com>
2019-06-02 08:23:29 +0200

    añadido efectos hover details e información general

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbComponent implements OnInit {
 
-  constructor() { }
+ public clicked : boolean;
+  constructor() {
+    this.clicked = true;
+   }
 
   ngOnInit() {
+    this.clicked = true;
   }
 
 }
