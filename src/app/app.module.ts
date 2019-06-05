fd5d515758a13563ca1c8c87e844c910aa2584b1
Cris <csimonvalero@gmail.com>
2019-06-05 15:00:08 +0200

    added before merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import  { UserComponent } from './shared/components/usernav/usernav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
