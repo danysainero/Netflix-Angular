822121d6b25eca4c468fd54df803e8621d6b4542
danysainero <danysainero@gmail.com>
2019-06-05 15:14:34 +0200

    conflictos ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoComponent } from './shared/components/video/video.component';
 import { CarouselComponent } from './shared/components/carousel/carousel.component';
-import { NgImageSliderModule } from 'ng-image-slider';
 import { MediaComponent } from './shared/components/media/media.component';
 import { ServiceService } from './shared/services/service.service';
-=======
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceService } from './shared/services/service.service';
 
-
->>>>>>> details
 
 @NgModule({
   declarations: [
@@ -28,16 +19,10 @@ import { ServiceService } from './shared/services/service.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    NgImageSliderModule,
   ],
   providers: [
     ServiceService
   ],
-=======
-  ],
-  providers: [ServiceService],
->>>>>>> details
   bootstrap: [AppComponent]
 })
 export class AppModule { }
